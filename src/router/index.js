66dab1de76240206b6cc75c9ea5f3d32ad89ff99
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-01-15 15:28:34 +0200

    Add Folders & Routes Structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import AdminRoutes from "../router/routes/AdminRoutes/index";
 
 Vue.use(VueRouter);
 
-const routes = [
+const baseRoutes = [
   {
     path: "/",
     name: "Home",
     component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
+
+const routes = [...baseRoutes, ...AdminRoutes]
 
 const router = new VueRouter({
   mode: "history",
---
+++ src/router/routes/AdminRoutes/index.js
@@ -0,0 +1,9 @@
+import Admin from '../../../views/AdminViews/index.vue'
+
+export default [
+    {
+        path: "/admin",
+        component: Admin,
+        name: "admin"
+    }
+]
